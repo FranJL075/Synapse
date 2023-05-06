294b1bedba308b26721cb3fc391fdda6fb62ba2f
Cintia Osorio Duarte <88154169+CintiaOsorioDuartesakura@users.noreply.github.com>
2023-05-05 21:50:03 -0300

    Creacion de modulos

--- Frontend/synapse/src/app/app.module.ts
+++ Frontend/synapse/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaerComponent } from './heaer/heaer.component';
+import { HeadeComponent } from './heade/heade.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaerComponent, HeadeComponent, HeaderComponent, FooterComponent, NavbarComponent, SectionComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
