a52472f4129fd98fa31f92810464c2bcfe2731aa
evera8 <evera8@hotmail.com>
2023-05-17 17:33:28 -0300

    Subiendo Maquetacion EcommerApp3

---
+++ MaquetacionApp3/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'product/:id', component: ProductComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
+  },
+  {
+    path: 'thankyou', component: ThankyouComponent
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
