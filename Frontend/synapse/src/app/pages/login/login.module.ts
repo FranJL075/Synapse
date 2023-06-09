95bbd64d96897d04e0519a9fb13819fb1d540310
isaias rub <106613100+isaias4912@users.noreply.github.com>
2023-06-08 21:34:02 -0300

    se agrega login

--- Frontend/synapse/src/app/pages/login/login.module.ts
+++ Frontend/synapse/src/app/pages/login/login.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    LoginRoutingModule,
+  ],
 })
-export class LoginModule {}
+export class LoginModule { }
